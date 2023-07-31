c138eced979cebdb2f257916dc1f32e3389ed3da
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-31 12:57:00 +0200

    llamada api

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,10 @@
-import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
-import responseMovies from "./mocks/with-results.json";
+import { useSearch } from "./hooks/useSearch";
 
-function useSearch() {
-  const [search, updateSearch] = useState("");
-  const [error, setError] = useState(null);
-  const isFirstInput = useRef(true);
-
-  useEffect(() => {
-    if (isFirstInput.current) {
-      isFirstInput.current = search === "";
-      return;
-    }
-    if (search === "") {
-      setError("No se puede buscar una película vacía");
-      return;
-    }
-
-    if (search.length < 3) {
-      setError("La búsqueda debe tener al menos 3 caracteres");
-      return;
-    }
-
-    setError(null);
-  }, [search]);
-
-  return { search, updateSearch, error };
-}
 function App() {
   const { search, updateSearch } = useSearch();
-  const movies = responseMovies.Search;
-
-  const mappedMovies = movies.map((movie) => {
-    return {
-      id: movie.imdbID,
-      title: movie.Title,
-      year: movie.Year,
-      poster: movie.Poster,
-    };
-  });
-
+  
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(search);
@@ -68,7 +32,7 @@ function App() {
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   );
