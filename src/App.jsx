891fb55044eb7d177a280c7082fe8eef93166dc0
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-31 12:33:27 +0200

    render movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-
-import './App.css'
+import "./App.css";
+import responseMovies from "./mocks/with-results.json";
 
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
 
   return (
-    <div className='page'>
-    
+    <div className="page">
+      <header>
+        <h1>React App</h1>
+        <form>
+          <input type="text" placeholder="Avengers, matrix..." />
+          <button>Search</button>
+        </form>
+      </header>
+
+      <main>
+        {hasMovies ? (
+          <ul className="movies">
+            {movies.map((movie) => (
+              <li className="movie" key={movie.imdbID}>
+                <img src={movie.Poster} alt={movie.Title} />
+                <h2>{movie.Title}</h2>
+                <p>{movie.Year}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No movies found</p>
+        )}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
