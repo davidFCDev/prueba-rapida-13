cea2ae598317dd073b205e31c7c8db5d0a89c4d2
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-31 12:43:16 +0200

    eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
+import { useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
 
 function App() {
+  const [search, updateSearch] = useState("");
   const movies = responseMovies.Search;
+
+  const mappedMovies = movies.map((movie) => {
+    return {
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      poster: movie.Poster,
+    };
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+    console.log(newSearch);
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+    console.log(newSearch);
+  };
 
   return (
     <div className="page">
       <header>
         <h1>React App</h1>
-        <form>
-          <input type="text" placeholder="Avengers, matrix..." />
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            name="query"
+            value={search}
+            type="text"
+            placeholder="Avengers, matrix..."
+          />
           <button>Search</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={movies} />
+        <Movies movies={mappedMovies} />
       </main>
     </div>
   );
